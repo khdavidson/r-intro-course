75cf21ad5d596c371c17e14d53637c6c49c1c4be
Davidson <DAVIDSONKA@dfo-mpo.ca>
2020-05-06 10:27:08 -0700

    initial commit refs

---
+++ week5-tidyverse1-KD.R
@@ -0,0 +1,8 @@
+# week 5
+
+
+# references
+# https://tidyverse-intro.github.io/3-dplyr.html#dplyr
+# https://www.analyticsvidhya.com/blog/2019/05/beginner-guide-tidyverse-most-powerful-collection-r-packages-data-science/
+
+
